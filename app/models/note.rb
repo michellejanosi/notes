e4f5421ee6781c6bc41809b79af7ac1063165a27
michellejanosi <michelle.janosi@gmail.com>
2017-07-15 11:48:14 -0400

    Added validation and tests

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,4 @@
 class Note < ApplicationRecord
+  validates :title, :notes, presence: true
+  validates :title, length: { maximum: 100 }
 end
--- test/models/note_test.rb
+++ test/models/note_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class NoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'fixtures are valid' do
+    assert notes(:one).valid?
+    assert notes(:two).valid?
+  end
+
+  test 'requires title' do
+    note = notes(:one)
+    note.title = ''
+    assert_not note.valid?
+  end
+
+  test 'requires notes' do
+    note = notes(:one)
+    note.notes = ''
+    assert_not note.valid?
+  end
+
+  test "title should be less than 100 characters" do
+    note = notes(:one)
+    note.title = '0' * 101
+    assert_not note.valid?
+  end
 end
